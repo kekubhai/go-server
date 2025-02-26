00f9d3176b3bf95b74031a6162ce2220495763ef
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-02-26 23:36:26 +0530

    Refactor load balancer implementation to use a simple server structure and improve error handling

--- loadbalancer/main.go
+++ loadbalancer/main.go
@@ -2,53 +2,45 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 )
 
-// Server represents a backend server
-type Server struct {
-	URL          *url.URL
-	Alive        bool
-	ReverseProxy *httputil.ReverseProxy
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, r *http.Request)
 }
 
-// List of backend servers
-var servers = []*Server{}
-
-func main() {
-	// Initialize backend servers
-	backends := []string{
-		"http://localhost:8081",
-		"http://localhost:8082",
+func newSimpleServer(add string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
 	}
 
-	// Setup backend servers
-	for _, backend := range backends {
-		url, _ := url.Parse(backend)
-		servers = append(servers, &Server{
-			URL:          url,
-			Alive:        true,
-			ReverseProxy: httputil.NewSingleHostReverseProxy(url),
-		})
-	}
+}
 
-	// Create a simple round-robin load balancer
-	currentServer := 0
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if len(servers) > 0 {
-			servers[currentServer].ReverseProxy.ServeHTTP(w, r)
-			currentServer = (currentServer + 1) % len(servers)
-		} else {
-			http.Error(w, "No backends available", http.StatusServiceUnavailable)
-		}
-	})
-
-	// Start the load balancer on port 8080
-	fmt.Println("Load Balancer started on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+type Loadbalancer struct {
+	port           string
+	rounRobinCount int
+	servers        []Server
+}
+func NewLoad  {
+	port string
+	roundRobin int
+	servers []Server
+}
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error : %v\n", err)
+		os.Exit(1)
 	}
 }
